473c945c9534ff0a171a5549a29848ce14bed10e
iLyhaha <gollfpd@gmail.com>
2025-12-10 01:54:58 +0200

    add footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import "../Footer/style.css";
 
 export function Footer() {
-  return <footer>
-    <div className="container">
-      1
-    </div>
-  </footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-left-column">
+          <div className="footer-logo">
+            <Image src={"/logo.svg"} width={300} height={60} alt="Bolts-Nuts" />
+          </div>
+          <div className="footer-left-column-content"></div>
+        </div>
+      </div>
+    </footer>
+  );
 }
