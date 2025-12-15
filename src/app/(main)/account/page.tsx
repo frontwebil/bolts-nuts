128e64702b4392a6eca6e029c53a31e1f87115f0
iLyhaha <gollfpd@gmail.com>
2025-12-15 02:18:08 +0200

    update

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
 export default function AccountPage() {
-  return <div>page</div>;
+  return (
+    <section className="AccountPage">
+      <div className="container">
+        <div
+          className="my-10 p-5 bg-amber-100 w-max"
+          onClick={() => signOut({ callbackUrl: "/" })}
+        >
+          logout
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/layout/Header/Components/PersonalData/PersonalAccount.tsx
+++ src/components/layout/Header/Components/PersonalData/PersonalAccount.tsx
@@ -4,18 +4,24 @@ import {
   setAuthOption,
 } from "@/redux/main/slices/uiSlice";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { PiUserCircle } from "react-icons/pi";
 import { useDispatch } from "react-redux";
 
 export function PersonalAccount() {
   const dispatch = useDispatch();
   const { data, status } = useSession();
+  const router = useRouter();
 
   return (
     <div className="header-personalAccount-data-column">
       <div
         className="header-personal-data-top-row"
         onClick={() => {
+          if (status === "authenticated") {
+            router.replace("/account");
+            return;
+          }
           dispatch(setAuthOption("login"));
           dispatch(openAuthModal());
           dispatch(closeBurger());
