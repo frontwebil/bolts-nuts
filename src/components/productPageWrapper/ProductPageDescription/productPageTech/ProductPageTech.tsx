2586d55bea6cc3043bfbd76f36fd151a9d546d6e
iLyhaha <gollfpd@gmail.com>
2026-01-04 23:42:25 +0200

    update product card

--- src/components/productPageWrapper/ProductPageDescription/productPageTech/ProductPageTech.tsx
+++ src/components/productPageWrapper/ProductPageDescription/productPageTech/ProductPageTech.tsx
@@ -1,26 +1,39 @@
 import { useSelector } from "react-redux";
 import "./style.css";
 import { RootState } from "@/redux/main/store";
+import Image from "next/image";
 
 export function ProductPageTech() {
   const { currentProduct } = useSelector(
     (store: RootState) => store.productSlice
   );
   return (
-    <div className="ProductPageTech-wrapper">
-      <div className="ProductPageTech-container">
-        <div className="ProductPageTech-top">
-          <h2>Technical Specs</h2>
-        </div>
-        <div className="ProductPageTech-container-grid">
-          {currentProduct?.specs.map((el, i) => (
-            <div className="ProductPageTech-container-grid-specs" key={i}>
-              <h3>{el.key}</h3>
-              <p>{el.value}</p>
-            </div>
-          ))}
+    <div className="ProductPageTech-wrapper-column">
+      <div className="ProductPageTech-wrapper">
+        <div className="ProductPageTech-container">
+          <div className="ProductPageTech-top">
+            <h2>Technical Specs</h2>
+          </div>
+          <div className="ProductPageTech-container-grid">
+            {currentProduct?.specs.map((el, i) => (
+              <div className="ProductPageTech-container-grid-specs" key={i}>
+                <h3>{el.key}</h3>
+                <p>{el.value}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+      {currentProduct?.technicalImg && (
+        <div className="tech-img">
+          <Image
+            src={currentProduct?.technicalImg}
+            width={1000}
+            height={5000}
+            alt="technical-img"
+          />
+        </div>
+      )}
     </div>
   );
 }
