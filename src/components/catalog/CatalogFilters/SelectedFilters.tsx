9bcba0cc55725ff1e172e3ac73ed27e85faef3c3
iLyhaha <gollfpd@gmail.com>
2026-01-06 21:58:26 +0200

    update

--- src/components/catalog/CatalogFilters/SelectedFilters.tsx
+++ src/components/catalog/CatalogFilters/SelectedFilters.tsx
@@ -1,10 +1,13 @@
-import { setSelectedCategory } from "@/redux/main/slices/productSlice";
+import {
+  setSelectedCategory,
+  toggleSpecValue,
+} from "@/redux/main/slices/productSlice";
 import { RootState } from "@/redux/main/store";
 import { IoClose } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 
 export function SelectedFilters() {
-  const { selectedCategory } = useSelector(
+  const { selectedCategory, selectedSpecs } = useSelector(
     (store: RootState) => store.productSlice
   );
   const dispatch = useDispatch();
@@ -27,6 +30,23 @@ export function SelectedFilters() {
           {selectedCategory}
           <IoClose />
         </button>
+        {Object.entries(selectedSpecs).map((row, i) => {
+          const label = row[0];
+          const values = row[1];
+
+          return values.map((el, index) => (
+            <button
+              key={index}
+              onClick={() =>
+                dispatch(toggleSpecValue({ key: label, value: el }))
+              }
+              className="filter-selected-content-button-wrapper-button"
+            >
+              {el}
+              <IoClose />
+            </button>
+          ));
+        })}
       </div>
     </div>
   );
