97fbdf76c1eb2c690656c0633f590b8974bb2635
iLyhaha <gollpfd@gmail.com>
2026-01-25 13:42:07 +0200

    Add Send Email to User

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -2,6 +2,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import prisma from "@/lib/prisma";
+import { sendMail } from "@/lib/nodemailer/sendmail";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-12-15.clover",
@@ -43,13 +44,136 @@ export async function POST(req: Request) {
 
       if (!orderId) break;
 
-      await prisma.order.update({
+      const order = await prisma.order.update({
         where: { id: orderId },
         data: {
           status: "paid",
           paymentIntentId: session.payment_intent as string,
           paidAt: new Date(),
         },
+        select: {
+          email: true,
+          name: true,
+          surname: true,
+        },
+      });
+
+      const baseUrl =
+        process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+
+      const cabinetLink = `${baseUrl}/account/orders`;
+
+      const fullName = `${order.name} ${order.surname}`;
+
+      const html = `
+<p style="
+margin: 0 0 12px;
+font-size: 15px;
+color: #111827;
+">
+Hello ${fullName},
+</p>
+
+
+<p style="
+margin: 0 0 18px;
+font-size: 14px;
+color: #374151;
+line-height: 1.6;
+">
+We’re happy to inform you that your payment has been successfully processed.
+</p>
+
+
+<div style="
+background: #f9fafb;
+border: 1px solid #e5e7eb;
+border-radius: 10px;
+padding: 16px 18px;
+margin-bottom: 24px;
+">
+<p style="
+margin: 0 0 6px;
+font-size: 13px;
+color: #6b7280;
+">
+Order status
+</p>
+<p style="
+margin: 0;
+font-size: 14px;
+font-weight: 600;
+color: #16a34a;
+">
+Paid
+</p>
+</div>
+
+
+<p style="
+margin: 0 0 22px;
+font-size: 14px;
+color: #374151;
+line-height: 1.6;
+">
+You can view the details of your order in your personal account using the email address
+you provided when placing the order.
+</p>
+
+
+<div style="text-align: center; margin-bottom: 24px;">
+<a href="${cabinetLink}" style="
+display: inline-block;
+background: #16a34a;
+color: #ffffff;
+padding: 13px 30px;
+border-radius: 8px;
+text-decoration: none;
+font-size: 14px;
+font-weight: 600;
+">
+View my orders
+</a>
+</div>
+
+
+<p style="
+margin: 0;
+font-size: 13px;
+color: #6b7280;
+line-height: 1.6;
+">
+If you have any questions about your order, simply reply to this email and our support team
+will be happy to assist you.
+</p>
+</div>
+
+
+<!-- Footer -->
+<div style="
+background: #f9fafb;
+padding: 18px;
+text-align: center;
+border-top: 1px solid #e5e7eb;
+">
+<p style="
+margin: 0;
+font-size: 12px;
+color: #9ca3af;
+">
+© ${new Date().getFullYear()} Clave. All rights reserved.
+</p>
+</div>
+
+
+</div>
+</div>
+`;
+
+      await sendMail({
+        to: order.email, // пошта з замовлення
+        subject: "Your payment was successful",
+        html,
       });
 
       console.log("✅ Order paid:", orderId);
@@ -66,11 +190,11 @@ export async function POST(req: Request) {
       await prisma.order.update({
         where: { id: orderId },
         data: {
-          status: "cancelled",
+          status: "expired",
         },
       });
 
-      console.log("⚠️ Order cancelled (session expired):", orderId);
+      console.log("⚠️ Order expired (session expired):", orderId);
       break;
     }
 
--- src/components/orderPage/OrderLayout/OrderShipping/OrderShippingCheckoutButton.tsx
+++ src/components/orderPage/OrderLayout/OrderShipping/OrderShippingCheckoutButton.tsx
@@ -1,5 +1,6 @@
 import { buildCartItemsDetailed } from "@/hooks/buildCartItems";
 import { RootState } from "@/redux/main/store";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 export function OrderShippingCheckoutButton() {
@@ -10,8 +11,10 @@ export function OrderShippingCheckoutButton() {
     shippingAddress,
     shippingName,
     userData,
+    stateCode,
   } = useSelector((store: RootState) => store.orderCartSlice);
   const { products } = useSelector((store: RootState) => store.productSlice);
+  const [loading, setLoading] = useState(false);
 
   const cartItemsDetailed = buildCartItemsDetailed(products, orderProducts);
   console.log(cartItemsDetailed);
@@ -22,6 +25,27 @@ export function OrderShippingCheckoutButton() {
   );
 
   const handleCheckout = async () => {
+    const validateForm = () => {
+      if (!userData?.email) return true;
+      if (!userData?.name) return true;
+      if (!userData?.surname) return true;
+      if (!userData?.phoneNumber) return true;
+
+      if (!shippingAddress?.postalCode) return true;
+      if (!shippingAddress?.city) return true;
+      if (!shippingAddress?.province) return true;
+      if (!shippingAddress?.address) return true;
+
+      if (gstHst === 0) return true;
+      if (shippingPrice === 0) return true;
+      if (stateCode === "") return true;
+
+      return false;
+    };
+    if (loading || validateForm()) return;
+
+    setLoading(true);
+
     const tax = subTotal * gstHst;
 
     const res = await fetch("/api/stripe/create-checkout-session", {
@@ -40,11 +64,14 @@ export function OrderShippingCheckoutButton() {
 
     const { url } = await res.json();
     window.location.href = url;
+
+    setLoading(false);
   };
 
   return (
     <button
       className="OrderLayout-button-next"
+      disabled={loading}
       onClick={() => handleCheckout()}
     >
       Pay Now
